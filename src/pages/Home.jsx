8e2acfcb888abeef7ed4ec6872caf224737f5fc7
Rexon_Dmello <rexon.dmello@costrategix.com>
2023-04-02 21:55:54 +0530

    app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import UserSearch from "../components/users/UserSearch"
+import UsersResults from "../components/users/UsersResults"
+export default function Home() {
+  return ( 
+    <>
+      <UserSearch/>
+      <UsersResults />
+    </>
+  )
+}
